fbd6767945dbb34ce563fe09735fa0297484813a
Nadeesha Pathiraja <pdncpathiraja95@gmail.com>
2021-09-14 01:06:41 +0530

    Splash screen done

--- lib/screens/splash_screen/splash_screen.dart
+++ lib/screens/splash_screen/splash_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:food_app/screens/splash_screen/getting_started.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({Key? key}) : super(key: key);
@@ -9,12 +10,44 @@ class SplashScreen extends StatefulWidget {
 
 class _SplashScreenState extends State<SplashScreen> {
   @override
+  void initState() {
+    // ignore: todo
+    // TODO: implement initState
+    super.initState();
+    Future.delayed(
+      const Duration(seconds: 5),
+      () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => const GettingStarted(),
+          ),
+        );
+      },
+    );
+  }
+
+  @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
     return Scaffold(
+      // ignore: sized_box_for_whitespace
       body: Container(
         width: size.width,
-        child: Column(),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset('assets/images/food.png'),
+            const SizedBox(height: 8),
+            const Text(
+              "No waiting for food",
+              style: TextStyle(
+                fontSize: 14.0,
+                color: Color(0xff838383),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
