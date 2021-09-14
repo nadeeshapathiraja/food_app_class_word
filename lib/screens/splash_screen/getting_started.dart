0f5c7173989b20bbf55355001dd2483c002b56bb
Nadeesha Pathiraja <pdncpathiraja95@gmail.com>
2021-09-14 22:05:00 +0530

    Getting stated page final view done

--- lib/screens/splash_screen/getting_started.dart
+++ lib/screens/splash_screen/getting_started.dart
@@ -13,11 +13,29 @@ class GettingStarted extends StatefulWidget {
 }
 
 class _GettingStartedState extends State<GettingStarted> {
-  CarouselController buttonCarouselController = CarouselController();
+  CarouselController carouselController = CarouselController();
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
-    List list = [1, 2, 3];
+    List<Widget> list = [
+      const SliderItem(
+        imgName: "popcorn.png",
+        title: "Choose A Tasty Dish",
+        discription: "Order anything you want from your\n Favorite restaurant.",
+      ),
+      const SliderItem(
+        imgName: "money.png",
+        title: "Easy Payment",
+        discription:
+            "Payment made easy through debit\n card, credit card  & more ways to pay\n for your food",
+      ),
+      const SliderItem(
+        imgName: "restaurant.png",
+        title: "Enjoy the Taste!",
+        discription:
+            "Healthy eating means eating a variety\n of foods that give you the nutrients you\n need to maintain your health.",
+      ),
+    ];
     int _current = 0;
     return Scaffold(
       body: Container(
@@ -25,71 +43,97 @@ class _GettingStartedState extends State<GettingStarted> {
           mainAxisAlignment: MainAxisAlignment.end,
           children: [
             CarouselSlider(
-              options: CarouselOptions(height: size.height / 2),
+              carouselController: carouselController,
+              options: CarouselOptions(
+                  height: size.height / 2,
+                  onPageChanged: (index, reason) {
+                    setState(() {
+                      _current = index;
+                    });
+                  }),
               items: list.map((i) {
                 return Builder(
                   builder: (BuildContext context) {
                     return Container(
                       child: Column(
-                        children: [
-                          Image.asset(
-                            Constants.imageAssets("popcorn.png"),
-                          ),
-                          const SizedBox(height: 37.0),
-                          const Text(
-                            "Choose A Tasty Dish",
-                            style: TextStyle(
-                              fontSize: 22.0,
-                              color: fontColor,
-                              fontWeight: FontWeight.w500,
-                            ),
-                          ),
-                          const SizedBox(height: 5.0),
-                          const Text(
-                            "Order anything you want from your\n Favorite restaurant.",
-                            textAlign: TextAlign.center,
-                            style: TextStyle(
-                              fontSize: 14.0,
-                              fontWeight: FontWeight.w400,
-                            ),
-                          ),
-                          const SizedBox(height: 67.0),
-                          //getting stated screen dots
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: list.asMap().entries.map((entry) {
-                              return GestureDetector(
-                                onTap: () => buttonCarouselController
-                                    .animateToPage(entry.key),
-                                child: Container(
-                                  width: 12.0,
-                                  height: 12.0,
-                                  margin: const EdgeInsets.symmetric(
-                                      vertical: 8.0, horizontal: 4.0),
-                                  decoration: BoxDecoration(
-                                      shape: BoxShape.circle,
-                                      color: (Theme.of(context).brightness ==
-                                                  Brightness.dark
-                                              ? Colors.white
-                                              : Colors.black)
-                                          .withOpacity(_current == entry.key
-                                              ? 0.9
-                                              : 0.4)),
-                                ),
-                              );
-                            }).toList(),
-                          ),
-                        ],
+                        // ignore: prefer_const_literals_to_create_immutables
+                        children: [i],
                       ),
                     );
                   },
                 );
               }).toList(),
             ),
-            BottomSection(size: size),
+
+            //getting stated screen dots
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: list.asMap().entries.map((entry) {
+                return GestureDetector(
+                  onTap: () => carouselController.animateToPage(entry.key),
+                  child: Container(
+                    width: 12.0,
+                    height: 12.0,
+                    margin: const EdgeInsets.symmetric(
+                        vertical: 8.0, horizontal: 4.0),
+                    decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        color:
+                            _current == entry.key ? primaryColor : greyColor),
+                  ),
+                );
+              }).toList(),
+            ),
+            BottomSection(
+              size: size,
+              ontap: () => carouselController.nextPage(),
+            ),
           ],
         ),
       ),
+    );
+  }
+}
+
+//Slider Items
+class SliderItem extends StatelessWidget {
+  const SliderItem({
+    Key? key,
+    required this.imgName,
+    required this.title,
+    required this.discription,
+  }) : super(key: key);
+
+  final String imgName;
+  final String title;
+  final String discription;
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Image.asset(
+          Constants.imageAssets(imgName),
+        ),
+        const SizedBox(height: 37.0),
+        Text(
+          title,
+          style: const TextStyle(
+            fontSize: 22.0,
+            color: fontColor,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+        const SizedBox(height: 5.0),
+        Text(
+          discription,
+          textAlign: TextAlign.center,
+          style: const TextStyle(
+            fontSize: 14.0,
+            fontWeight: FontWeight.w400,
+          ),
+        ),
+        const SizedBox(height: 67.0),
+      ],
     );
   }
 }
@@ -98,9 +142,11 @@ class BottomSection extends StatelessWidget {
   const BottomSection({
     Key? key,
     required this.size,
+    required this.ontap,
   }) : super(key: key);
 
   final Size size;
+  final Function() ontap;
 
   @override
   Widget build(BuildContext context) {
@@ -121,7 +167,7 @@ class BottomSection extends StatelessWidget {
               mainAxisAlignment: MainAxisAlignment.end,
               children: [
                 ElevatedButton(
-                  onPressed: () {},
+                  onPressed: ontap,
                   child: const Text(
                     "Next",
                     style: TextStyle(
